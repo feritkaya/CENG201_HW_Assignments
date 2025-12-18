1c7fde9bdabb80adbc82821d83c59fb52ec61784
Ferit Kaya <s230444006@stu.thk.edu.tr>
2025-12-18 16:27:59 +0300

    Task3:DischargeRecord and DischargeStack classes created

--- src/DischargeStack.java
+++ src/DischargeStack.java
@@ -1,2 +1,62 @@
 public class DischargeStack {
+    Node top = null;;
+    int size = 0;
+
+    class Node {
+        DischargeRecord data;
+        Node next;
+
+        Node() {
+            this.data = null;
+            this.next = null;
+        }
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    void push(DischargeRecord d1) {
+        Node node = new Node();
+        node.data = d1;
+
+        if (isEmpty()){
+            top = node;
+        } else {
+            node.next = top;
+            top = node;
+        }
+        size++;
+    }
+
+    DischargeRecord pop(){
+        if (isEmpty()){
+            return null;
+        }else {
+            DischargeRecord current = top.data;
+            top = top.next;
+            size--;
+            return current;
+        }
+    }
+
+    DischargeRecord peek(){
+        if (isEmpty()){
+            return null;
+        }else {
+            return top.data;
+        }
+    }
+
+    void printStack(){
+        if (isEmpty()){
+            System.out.println("EMPTY");
+            return;
+        }
+        Node current = top;
+        while (current != null){
+            System.out.println("Patient ID: " + current.data.patientId + ", Discharge Time: " + current.data.dischargeTime);
+            current = current.next;
+        }
+    }
 }
