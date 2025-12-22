0599e944d4f89a931d778db41ae0bdc406da0636
Ferit Kaya <s230444006@stu.thk.edu.tr>
2025-12-22 22:09:54 +0300

    update

--- src/TreatmentQueue.java
+++ src/TreatmentQueue.java
@@ -1,9 +1,9 @@
 public class TreatmentQueue {
-    Node front = null; // head
-    Node rear = null;  // tail
+    Node front = null; // head of the queue
+    Node rear = null;  // tail of the queue
     int size = 0;
 
-    class Node {
+    class Node { // node class
         TreatmentRequest data;
         Node next;
 
@@ -14,15 +14,15 @@ public class TreatmentQueue {
     }
 
     void enqueue(TreatmentRequest t2) {
-        Node node = new Node();
-        node.data = t2;
+        Node node = new Node(); // generate new node
+        node.data = t2; // define data
 
 
-        if (isEmpty()) {
-            front = node;
+        if (isEmpty()) { // check if the queue is empty
+            front = node; // the newly added element becomes both front and rear
             rear = node;
             System.out.println("First request added.");
-        } else {
+        } else { // the next reference of the current last element links to the new node
             rear.next = node;
             rear = node;
             System.out.println("Request added.");
@@ -30,19 +30,20 @@ public class TreatmentQueue {
         size++;
     }
 
-    private boolean isEmpty() {
+    private boolean isEmpty() { // check the occupancy of the queue
         return size == 0;
     }
 
     TreatmentRequest dequeue() {
-        if (isEmpty()) {
+        if (isEmpty()) { // check if the queue is empty
             System.out.println("The queue is empty.");
             return null;
         }
-        TreatmentRequest current = front.data;
-        front = front.next;
-        size--;
+        TreatmentRequest current = front.data; // assign the data(front) to a temporary variable
+        front = front.next; // move the front to the next node
+        size--; // reduce the size
 
+        // ıf the last element is removed and front becomes null, rear must also be null
         if (front == null) {
             rear = null;
         }
@@ -50,20 +51,21 @@ public class TreatmentQueue {
         return current;
     }
 
-    int size() {
+    int size() { // return the size of the queue
         return size;
     }
 
     void printQueue() {
-        if (isEmpty()) {
+        if (isEmpty()) { // check if the queue is empty
             System.out.println("The queue is empty.");
             return;
         }
-        Node current = front;
+        Node current = front; // traverse and start from the front
         System.out.println("FRONT");
-        while (current != null) {
+        while (current != null) { // go through all the nodes one by one
+            // print the data of the current node
             System.out.println("Patient ID: " + current.data.patientId + ", arrivalTime: " + current.data.arrivalTime);
-            current = current.next;
+            current = current.next; // move on to the next node
         }
         System.out.println("REAR");
     }
--- src/TreatmentRequest.java
+++ src/TreatmentRequest.java
@@ -1,8 +1,8 @@
-public class TreatmentRequest {
-    int patientId;
-    long arrivalTime;
+public class TreatmentRequest { // treatment request class
+    int patientId; // patient ID
+    long arrivalTime; // arrival time
 
-    public TreatmentRequest(int patientId) {
+    public TreatmentRequest(int patientId) { // constructor method
         this.patientId = patientId;
         this.arrivalTime = System.currentTimeMillis();
     }
