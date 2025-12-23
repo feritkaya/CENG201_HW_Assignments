562ac0fa1537e418d96f00122c654df0f3759cea
Ferit Kaya <s230444006@stu.thk.edu.tr>
2025-12-23 17:40:34 +0300

    Task4:HospitalSystem class created

--- src/HospitalSystem.java
+++ src/HospitalSystem.java
@@ -9,7 +9,10 @@ public class HospitalSystem {
     // using a HashMap for quick searching
     HashMap<Integer, Patient> patientMap = new HashMap<>();
 
-
+    void addPatient(Patient p) {
+        patients.addPatient(p);
+        patientMap.put(p.id, p);
+    }
 
 
 
