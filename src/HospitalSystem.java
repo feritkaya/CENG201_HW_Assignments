7b657715bef4b3445bbe43c84d7e30bd218bb4f8
Ferit Kaya <s230444006@stu.thk.edu.tr>
2025-12-23 17:37:10 +0300

    Task4:HospitalSystem class created

--- src/HospitalSystem.java
+++ src/HospitalSystem.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+
 public class HospitalSystem {
+    // lets move the classes from other tasks here and combine them
+    PatientList patients = new PatientList();
+    TreatmentQueue treatmentQueue = new TreatmentQueue();
+    TreatmentQueue priorityQueue = new TreatmentQueue(); // for priority patients
+    DischargeStack dischargeStack = new DischargeStack();
+    // using a HashMap for quick searching
+    HashMap<Integer, Patient> patientMap = new HashMap<>();
+
+
+
+
+
 }
