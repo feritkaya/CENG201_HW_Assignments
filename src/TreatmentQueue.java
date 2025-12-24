760e3d120cd257567e6ccb8fce45bac39c77deb0
Ferit Kaya <s230444006@stu.thk.edu.tr>
2025-12-24 22:04:24 +0300

    update

--- src/TreatmentQueue.java
+++ src/TreatmentQueue.java
@@ -1,9 +1,10 @@
 public class TreatmentQueue {
-    Node front = null; // head of the queue
-    Node rear = null;  // tail of the queue
+    Node front = null; // queue's head
+    Node rear = null;  // queue's tail
     int size = 0;
 
-    class Node { // node class
+    // node class
+    class Node {
         TreatmentRequest data;
         Node next;
 
@@ -14,59 +15,69 @@ public class TreatmentQueue {
     }
 
     void enqueue(TreatmentRequest t2) {
-        Node node = new Node(); // generate new node
+        Node node = new Node(); // generate node
         node.data = t2; // define data
 
-
-        if (isEmpty()) { // check if the queue is empty
-            front = node; // the newly added element becomes both front and rear
+        // queue is empty?
+        if (isEmpty()) {
+            front = node; // new added element becomes both front and rear
             rear = node;
-            System.out.println("First request added.");
-        } else { // the next reference of the current last element links to the new node
+            System.out.println("first request added.");
+
+        // current last element's next reference  links to new node
+        } else {
             rear.next = node;
             rear = node;
-            System.out.println("Request added.");
+            System.out.println("request added.");
         }
         size++;
     }
 
-    private boolean isEmpty() { // check the occupancy of the queue
+    // checking queue's occupancy
+    private boolean isEmpty() {
         return size == 0;
     }
 
     TreatmentRequest dequeue() {
-        if (isEmpty()) { // check if the queue is empty
-            System.out.println("The queue is empty.");
+        // queue is empty?
+        if (isEmpty()) {
+            System.out.println("queue is empty.");
             return null;
         }
-        TreatmentRequest current = front.data; // assign the data(front) to a temporary variable
-        front = front.next; // move the front to the next node
-        size--; // reduce the size
+        // assigning front to temporary variable
+        TreatmentRequest current = front.data;
+        front = front.next; // move front to next node
+        size--; // reduce size
 
-        // ıf the last element is removed and front becomes null, rear must also be null
+        // ıf last element is removed and front becomes null
+        // rear must be null
         if (front == null) {
             rear = null;
         }
-        System.out.println("Request completed and removed from the queue.");
+        System.out.println("request completed and removed from queue.");
         return current;
     }
 
-    int size() { // return the size of the queue
+    int size() { // return queue's size
         return size;
     }
 
     void printQueue() {
-        if (isEmpty()) { // check if the queue is empty
-            System.out.println("The queue is empty.");
+        // queue is empty?
+        if (isEmpty()) {
+            System.out.println("queue is empty.");
             return;
         }
-        Node current = front; // traverse and start from the front
-        System.out.println("FRONT");
-        while (current != null) { // go through all the nodes one by one
-            // print the data of the current node
-            System.out.println("Patient ID: " + current.data.patientId + ", arrivalTime: " + current.data.arrivalTime);
-            current = current.next; // move on to the next node
+        // traverse and start from front
+        Node current = front;
+        System.out.println("front");
+
+        // go all nodes one by one
+        while (current != null) {
+            // print current node's data
+            System.out.println("Patient ID: " + current.data.patientId + " arrivalTime: " + current.data.arrivalTime);
+            current = current.next; // move on to next node
         }
-        System.out.println("REAR");
+        System.out.println("rear");
     }
 }
--- src/TreatmentRequest.java
+++ src/TreatmentRequest.java
@@ -1,9 +1,11 @@
 public class TreatmentRequest { // treatment request class
     int patientId; // patient ID
     long arrivalTime; // arrival time
+    boolean urgency;
 
-    public TreatmentRequest(int patientId) { // constructor method
+    public TreatmentRequest(int patientId, boolean urgency) { // constructor method
         this.patientId = patientId;
         this.arrivalTime = System.currentTimeMillis();
+        this.urgency = urgency;
     }
 }
