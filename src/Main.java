d65255236d36e9badb157b8dd82b6298525b7584
Ferit Kaya <s230444006@stu.thk.edu.tr>
2025-12-25 14:23:33 +0300

    task4's analysis report have been written

--- src/Main.java
+++ src/Main.java
@@ -56,7 +56,10 @@ public class Main {
 //
 //        ds.printStack();
 
+//        ---TASK-4 TEST---
+
 //        HospitalSystem hs = new HospitalSystem();
+
 //        hs.patientAdmission(new Patient(700, "Elif", 6, 23));
 //        hs.patientAdmission(new Patient(701, "Ferit", 7, 21));
 //        hs.patientAdmission(new Patient(702, "Berkay", 14, 22));
@@ -67,31 +70,35 @@ public class Main {
 //        hs.patientAdmission(new Patient(707, "Talim", 3, 50));
 //        hs.patientAdmission(new Patient(708, "Özgür", 10, 41));
 //        hs.patientAdmission(new Patient(709, "Mustafa", 4, 47));
-//
+
+
 //        hs.patientTreatmentRequest(700, false);
 //        hs.patientTreatmentRequest(701, true);
 //        hs.patientTreatmentRequest(702, false);
 //        hs.patientTreatmentRequest(703, true);
 //        hs.patientTreatmentRequest(704, false);
-//
+
+
 //        hs.patientTreatmentRequest(706, true);
 //        hs.patientTreatmentRequest(709, true);
 //        hs.patientTreatmentRequest(708, true);
-//
+
+
 //        hs.patientDischarge(707);
 //        hs.patientDischarge(705);
-//
-//
+
+
 //        System.out.println("--- SORTING PATIENTS ---");
 //        hs.sortingPatients();
-//
+
+
 //        System.out.println("--- PROCESSING 4 REQUESTS ---");
 //        hs.patientProcess();
 //        hs.patientProcess();
 //        hs.patientProcess();
 //        hs.patientProcess();
-//
-//
+
+
 //        System.out.println("--- FINAL SYSTEM STATE ---");
 //        hs.printSystemState();
 
